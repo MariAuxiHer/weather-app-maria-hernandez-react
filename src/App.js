547231fcb25f84f84c8b2a531fb8eee98a993727
MariAuxiHer <72420601+MariAuxiHer@users.noreply.github.com>
2021-01-19 12:11:43 -0600

    I added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import SearchCity from "./SearchCity";
 import SpecificCities from "./SpecificCities";
 import CurrentInfo from "./CurrentInfo";
