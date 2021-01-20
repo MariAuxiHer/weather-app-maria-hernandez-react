0e8980151e9febe793b6fbfb7e5f16c2ce99e168
MariAuxiHer <72420601+MariAuxiHer@users.noreply.github.com>
2021-01-20 14:46:28 -0600

    Added some packages to package.json - Added useState in one of the components

--- src/SearchCity.js
+++ src/SearchCity.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchCity.css";
 
 export default function SearchCity() {
+let [city, setCity] = useState("");
+
+function displayCityName(event){
+  event.preventDefault();
+  alert(city);
+}
+
+function updateCity(event){
+  event.preventDefault();
+  setCity(event.target.value);
+}
+
+//let apiKey = "ac3c02e9439b2a5f701addd7d8527168";
+//let apiUrl = `api.openweathermap.org/data/2.5/weather?q={city}&appid={apiKey}`
+
+
   return (
     <div className="row">
       <div className="col-1">
@@ -9,9 +25,10 @@ export default function SearchCity() {
       </div>
 
       <div className="col-8">
-        <form id="submit-city">
+        <form id="submit-city" onSubmit={displayCityName}>
           <input
             type="search"
+            onChange={updateCity}
             placeholder="Type a City"
             id="enter-city"
             autoComplete="off"
